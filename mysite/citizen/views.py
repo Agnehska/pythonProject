5822da7b01eab63b6ede7dad7a2cca532d3ff9b0
Agneshka <tosya080116@gmail.com>
2022-07-23 11:43:12 +0300

    Fourth commit

--- mysite/citizen/views.py
+++ mysite/citizen/views.py
@@ -67,3 +67,47 @@ def create(request):
 def product_list(request):
     filter = ProductFilter(request.GET, queryset=People.objects.all())
     return render(request, 'citizen/citizen.html', {'filter': filter})
+
+
+# CRUD-операции
+# (venv) C:\Users\Свешникова Анна\Desktop\pythonProject3\mysite>python manage.py shell
+# Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# (InteractiveConsole)
+# >>> from citizen.models import People
+# >>> People(name='Петров Андрей', age='25', master='Иванов Иван')
+# <People: Петров Андрей>
+# >>> man1 = _
+# >>> man1
+# <People: Петров Андрей>
+# >>> man1.name
+# 'Петров Андрей'
+# >>> man1.age
+# '25'
+# >>> man1.save()
+# >>> man2 = People(name='Иванов Иван', age='25', master='')
+# >>> man2.save()
+# >>> man4 = People.objects.create(name='Artur', age='25', master='87')
+# >>> People.objects.all()
+# <QuerySet [<People: Anna Sveshnikova>, <People: Artur>, <People: Абрамов Аркадий>, <People: Авдеева Светлана>, <People: Агафонова Анна>, <People: Аксёнова Илена>, <People: Александров Арнольд>, <People: Александров Аскольд>, <People: А
+# лександров Денис>, <People: Александров Роман>, <People: Александрова Георгина>, <People: Александрова Лили>, <People: Александрова Пелагея>, <People: Анисимова Тамара>, <People: Антипов Платон>, <People: Антонов Артур>, <People: Антон
+# ова София>, <People: Апоян Давид>, <People: Ардаков Игорь>, <People: Архангельская Мария>, '...(remaining elements truncated)...']>
+# >>> people = _
+# >>> people
+# <QuerySet [<People: Anna Sveshnikova>, <People: Artur>, <People: Абрамов Аркадий>, <People: Авдеева Светлана>, <People: Агафонова Анна>, <People: Аксёнова Илена>, <People: Александров Арнольд>, <People: Александров Аскольд>, <People: А
+# лександров Денис>, <People: Александров Роман>, <People: Александрова Георгина>, <People: Александрова Лили>, <People: Александрова Пелагея>, <People: Анисимова Тамара>, <People: Антипов Платон>, <People: Антонов Артур>, <People: Антон
+# ова София>, <People: Апоян Давид>, <People: Ардаков Игорь>, <People: Архангельская Мария>, '...(remaining elements truncated)...']>
+# >>> for man in people:
+# ...     print(man.name)
+# ...
+# Anna Sveshnikova
+# Artur
+# Абрамов Аркадий
+# Авдеева Светлана
+# Агафонова Анна
+# Аксёнова Илена ……….
+# >>> man = People.objects.get(name='Artur')
+# >>> man.name='Артур'
+# >>> man.save()
+# >>> man = People.objects.get(name='Иванов Иван')
+# >>> man.delete()
